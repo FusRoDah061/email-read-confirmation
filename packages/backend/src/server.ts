f05ee871c0bd05f56a61578aa24d02274fa7ae49
Allex Rodrigues <allexxrodriguess@gmail.com>
2021-01-13 19:12:06 -0300

    fix: Enabling "trust proxy" for heroku deploy

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -13,6 +13,9 @@ app.use(express.json());
 app.use(errors());
 app.use(routes);
 
+// Heroku specific configuration
+app.enable('trust proxy');
+
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
   console.log(`Application listening on port ${port}`);
