4377e9fde5732a78ef617f876d032e98c431f5f1
Allex Rodrigues <allexxrodriguess@gmail.com>
2021-01-21 23:22:51 -0300

    feat: Initial page structuring

--- packages/frontend/src/styles.ts
+++ packages/frontend/src/styles.ts
@@ -1,3 +1,33 @@
 import styled from 'styled-components';
 
 export const Container = styled.main``;
+
+export const Jumbotron = styled.section``;
+
+export const StartButton = styled.button``;
+
+export const StepContainer = styled.section``;
+
+export const StepLabelColumn = styled.div``;
+
+export const StepLabel = styled.p``;
+
+export const StepFormColumn = styled.div``;
+
+export const InputWrapper = styled.div``;
+
+export const StepHint = styled.p``;
+
+export const StepDescription = styled.p``;
+
+export const Button = styled.button``;
+
+export const FinalStepContainer = styled.div``;
+
+export const FinalStepTitle = styled.p``;
+
+export const FinalStepHint = styled.p``;
+
+export const CodeSnippet = styled.div``;
+
+export const CodeSnippetCopyButton = styled.button``;
