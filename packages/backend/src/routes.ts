9368da6fbf25da28006a9060e227f20a93d0e41e
Allex Rodrigues <allexxrodriguess@gmail.com>
2021-01-25 19:26:19 -0300

    feat: Improve Joi validation error reporting

--- packages/backend/src/routes.ts
+++ packages/backend/src/routes.ts
@@ -8,13 +8,32 @@ const notificationController = new NotificationController();
 
 router.post(
   '/notify',
-  celebrate({
-    [Segments.BODY]: {
-      sender: Joi.string().email().trim().required(),
-      description: Joi.string().trim().max(100).required(),
-      recipient: Joi.string().trim().optional().allow('', null),
+  celebrate(
+    {
+      [Segments.BODY]: {
+        sender: Joi.string().email().trim().max(256).required().messages({
+          'string.empty': `The notification email field cannot be empty`,
+          'string.email': `Invalid notification email address`,
+          'any.required': `Notification email is required to send this notification`,
+          'string.max': `Email should not be longer than 256 characters!`,
+        }),
+        description: Joi.string().trim().max(100).required().messages({
+          'string.empty': `The description field cannot be empty`,
+          'string.max': `The description field should be longer than 100 characters!`,
+          'any.required': `A description is required`,
+        }),
+        recipient: Joi.string()
+          .trim()
+          .max(100)
+          .optional()
+          .allow('', null)
+          .messages({
+            'string.max': `The recipient filed should be longer than 100 characters!`,
+          }),
+      },
     },
-  }),
+    { abortEarly: false },
+  ),
   notificationController.create,
 );
 
